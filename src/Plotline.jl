445574af30fd8d0961f7abb7ac01bae1f703ce59
Sean Voisen <sean@voisen.org>
2025-04-24 20:59:59 -0700

    update deps, fix hatch export

--- src/Plotline.jl
+++ src/Plotline.jl
@@ -5,7 +5,7 @@ include("offset.jl")
 include("random.jl")
 include("splines.jl")
 
-export hatchfill, offset,
+export hatch, offset,
        pointcloud, randompointincircle,
        randompointsincircle, distpointsincircle,
        randompointinellipse, randompointsinellipse,
